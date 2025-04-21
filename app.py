1beae82608ca20af3b44ac89c3088b23db3c66af
amneshpal <amneshpal016@gmail.com>
2025-04-21 16:57:16 +0530

    Fix nltk for cloud + add requirements.txt

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-nltk.download('stopwords')
+import os
+nltk_data_path = os.path.join(os.path.expanduser('~'), 'nltk_data')
+nltk.download('stopwords', download_dir=nltk_data_path)
+nltk.data.path.append(nltk_data_path)
+
 
 # ---------- Helper Functions ----------
 
